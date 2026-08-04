ca7432b052978d5cdbdf7937359f1cb8a38a77b4
dvuvud <carldavid.buhrman@gmail.com>
2026-08-04 22:08:57 +0200

    use std::random_device for temp file name generation in tests

--- tests/test_helpers.hpp
+++ tests/test_helpers.hpp
@@ -1,12 +1,15 @@
 #pragma once
 
-#include <atomic>
 #include <filesystem>
+#include <format>
+#include <random>
 #include <string>
 
 inline std::string make_temp_path(std::string_view tag = "tmp") {
-  static std::atomic<uint32_t> counter{0};
+  static thread_local std::mt19937 gen{ [] { std::random_device rd; return rd(); }() };
+  const auto res = gen();
+  // if the temp directory path includes any non-ascii characters, the
+  // conversion from std::filesystem::path to std::string may mangle the path
   return (std::filesystem::temp_directory_path() /
-          ("fluxen_" + std::string(tag) + "_" + std::to_string(counter++)))
-      .string();
+          std::format("fluxen_{}_{:04X}-{:04X}", tag, res & 0xFFFF, (res >> 16) & 0xFFFF)).string();
 }
